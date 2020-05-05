9b8ae266923080b3dcbfe25826a407fca0123d65
Ganesh Tiwari <gtiwari333@gmail.com>
2020-05-05 18:11:23 -0500

    proxy to bypass cors

--- web/src/app/app.constants.ts
+++ web/src/app/app.constants.ts
@@ -1,5 +1,5 @@
 
 // uncomment the lines to use corrent api URL before running
 
-export const SERVER_API_URL = "http://localhost:8080/";
+export const SERVER_API_URL = "http://localhost:4200/server/";
 // export const SERVER_API_URL = "https://gt-quote-app.herokuapp.com/";
