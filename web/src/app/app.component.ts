091290a87fbbcade494a64e929a2deb1aa91ca1c
Ganesh Tiwari <gtiwari333@gmail.com>
2020-05-05 23:54:29 -0500

    logged in check

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,19 +1,34 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {KeycloakService} from 'keycloak-angular';
+import {Router} from '@angular/router';
+import {KeycloakProfile} from "keycloak-js";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'web';
-  isCollapsed = true;
+export class AppComponent implements OnInit {
+  userDetails: KeycloakProfile;
 
-
-  doLogin(): void {
+  constructor(private router: Router,
+              private keycloakService: KeycloakService) {
   }
 
-
   async doLogout() {
+    await this.keycloakService.logout();
+    await this.router.navigate(['/']);
+  }
+
+  doLogin(): void {
+    this.keycloakService.login();
+  }
+
+  async ngOnInit() {
+    if (await this.keycloakService.isLoggedIn()) {
+      this.userDetails = await this.keycloakService.loadUserProfile();
+      console.log(this.userDetails);
+    }
   }
 }
